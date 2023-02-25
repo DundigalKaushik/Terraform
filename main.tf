762b52b25214cbc3353c727170759309e6d7862b
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-25 12:43:50 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "test1" {
+  count = var.ec2_count
+  ami = var.ami_id
+  instance_type = var.instance_type
+#  key_name = "tf-key"
+# Security group association
+# security_groups = [aws_security_group.TF-SG.name]
+  
+  tags = {
+    Name = "test-instance"
+  }
+  
+}
