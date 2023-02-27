4c19c6111c85a78cffb7e153c3f132a314b2eac4
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-27 10:24:49 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main-1" {
     Name = "breeding1-vpc"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main-1.id
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "public-sub"
+  }
+}
