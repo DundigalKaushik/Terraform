a29e5f961975e4f99b1241a3ce5657d8bf27eee4
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-27 09:54:29 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/24"
+  cidr_block       = "10.143.128.0/27"
   instance_tenancy = "default"
 
   tags = {
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "igw" {
 }
 resource "aws_subnet" "public-sub" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.143.128.32/27"
 
   tags = {
     Name = "public-sub"
@@ -23,7 +23,7 @@ resource "aws_subnet" "public-sub" {
 }
 resource "aws_subnet" "private-sub" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.143.128.64/27"
 
   tags = {
     Name = "private-sub"
