4ae89c6acbe774de33547069939ace36f9ff77a4
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-27 11:09:56 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "test1" {
   count = var.ec2_count
   ami = var.ami_id
   instance_type = var.instance_type
+  subnet_id = aws_subnet.public-subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 #  key_name = "tf-key"
 # Security group association
 # security_groups = [aws_security_group.TF-SG.name]
