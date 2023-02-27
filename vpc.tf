5f8012c17800076495e61194086c2bef40e0958a
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-27 09:36:30 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -29,18 +29,7 @@ resource "aws_subnet" "private-sub" {
     Name = "private-sub"
   }
 }
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  
-  tags = {
-    Name = "main-rtb"
-  }
-}
 
   
 resource "aws_security_group" "allow_tls" {
