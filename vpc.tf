9bc69cf8aecfad58f475970ed95c8676a0639f94
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-27 10:40:39 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -16,3 +16,13 @@ resource "aws_subnet" "public-subnet" {
     Name = "public-subnet"
   }
 }
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id            = aws_vpc.main-1.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
