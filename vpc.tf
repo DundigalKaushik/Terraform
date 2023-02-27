342d28a0a28562360cd82666c821735af171ae64
DundigalKaushik <109131206+DundigalKaushik@users.noreply.github.com>
2023-02-27 11:00:44 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -53,3 +53,29 @@ resource "aws_route_table_association" "main-rtb-a" {
   subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.main-rtb.id
 }
+
+resource "aws_security_group" "web-sg" {
+  name   = "HTTP and SSH"
+  vpc_id = aws_vpc.main-1.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
